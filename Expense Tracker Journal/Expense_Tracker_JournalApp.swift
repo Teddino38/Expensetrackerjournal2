d562136bd3c1618d68e93c5f169f4d797058d815
Ted <raffaele.terracciano.777@gmail.com>
2024-12-09 12:03:16 +0100

    Initial Commit

---
+++ Expense Tracker Journal/Expense_Tracker_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_Tracker_JournalApp.swift
+//  Expense Tracker Journal
+//
+//  Created by Raffaele Terracciano on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Expense_Tracker_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
